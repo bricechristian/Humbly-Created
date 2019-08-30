523349819092bc9340ad54441a7e86c19c11cc15
Brice Christian <bricechristian93@gmail.com>
2019-08-30 16:14:38 -0400

    Added core and updated config

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -26,9 +26,7 @@ module.exports = {
   pathPrefix: '/gatsby-contentful-starter',
   plugins: [
     'gatsby-transformer-remark',
-    'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-source-contentful',
       options: contentfulConfig,
